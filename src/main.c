5cc84e48c85600aaaad43b328bb0d71ec753d322
“[Antoine <contact@techxplorer.fr>
2024-01-22 12:29:41 +0100

    Added file parsing and refactor
    Co-authored-by: BERNARD Max <maxbernard3@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ReadFile(char* filename, char* output, int size){
-    FILE* ptr;
-    char ch;
- 
-    ptr = fopen(filename, "r");
- 
-    if (NULL == ptr) {
-        printf("file can't be opened \n");
+int parse_content(const char *content)
+{
+    char str1[5], str2[10], str3[10];
+    int offset = 0, read;
+    while (sscanf(content + offset, "%s %s %s%n", str1, str2, str3, &read) == 3)
+    {
+        printf("%s\n%s\n%s\n", str1, str2, str3);
+        offset += read;
     }
-    
-    int count = 0;
+    return 0;
+}
 
-    do {
+int read_file(char *filename, char *output, int size)
+{
+    FILE *ptr;
+    char ch;
+    ptr = fopen(filename, "r");
+
+    if (NULL == ptr)
+    {
+        printf("file can't be opened \n");
+        return 1;
+    }
+
+    int count = 0;
+    while (!feof(ptr))
+    {
         ch = fgetc(ptr);
         output[count] = ch;
         count++;
-    } while (ch != EOF);
+    }
+
+    output[count - 1] = '\0';
 
     fclose(ptr);
     return 0;
@@ -47,17 +63,19 @@ int get_file_size(char *filename, int *size)
     return 0;
 }
 
-int main(){
+int main()
+{
     int i = 0;
     get_file_size("example.asm", &i);
 
     char *content;
     content = (char *)malloc(i * sizeof(char));
 
-    ReadFile("example.asm", content, i);
+    read_file("example.asm", content, i);
 
-    printf("%s", content);
+    // printf("%s", content);
 
+    parse_content(content);
 
     return 0;
 }
