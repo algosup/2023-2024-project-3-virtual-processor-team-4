87092cf59aa04d169d98ba597fa593a81fe0926c
maxbernard3 <max.bernard_@hotmail.com>
2024-02-12 12:25:45 +0100

    fix error with the 1st commit

--- src/libs/runtime.h
+++ src/libs/runtime.h
@@ -11,22 +11,22 @@ uint16_t registerArr[32];
 uint8_t memory[201326591];
 
 
-read_bin(char* inputFile, char* output){
+int read_bin(char* inputFile, char* output){
     FILE *f = fopen(inputFile, "rb");
 
     fseek(f, 0, SEEK_END);
-    long fileSize = ftell(f);
+    long size = ftell(f);
+    rewind(f);
 
-    char* output = (char*)malloc(sizeof(char)*size);
+    output = (char*)malloc(sizeof(char)*size);
 
-    if (ptr == NULL)
+    if (f == NULL)
     {
         printf("Failed to open file: %s\n\n", inputFile);
         return FILE_NOT_FOUND;
     }
 
-    fclose(ptr);
-
+    fclose(f);
 
     //memcpy(p, &node, sizeof(char));
     return SUCCESS;
