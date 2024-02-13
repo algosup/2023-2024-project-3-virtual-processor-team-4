c035b16f88ef95e90bc89b5bb71a6ac6ddd9ccd5
maxbernard3 <max.bernard_@hotmail.com>
2024-02-13 14:36:29 +0100

    working read_bin function

--- src/libs/runtime.h
+++ src/libs/runtime.h
@@ -11,24 +11,34 @@ uint16_t registerArr[32];
 uint8_t memory[201326591];
 
 
-int read_bin(char* inputFile, char* output){
+int read_bin(char* inputFile, uint8_t** output){
     FILE *f = fopen(inputFile, "rb");
+    if (f == NULL)
+    {
+        perror("Error: Failed to open file");
+        return FILE_NOT_FOUND;
+    }
 
     fseek(f, 0, SEEK_END);
     long size = ftell(f);
     rewind(f);
 
-    output = (char*)malloc(sizeof(char)*size);
+    *output = (uint8_t*)malloc(sizeof(uint8_t)*size);
+    if (*output == NULL) {
+        perror("Error: Memory allocation failed");
+        fclose(f);
+        return GENERIC_ERROR;
+    }
 
-    if (f == NULL)
-    {
-        printf("Failed to open file: %s\n\n", inputFile);
-        return FILE_NOT_FOUND;
+    size_t read = fread(*output, 1, size, f);
+    if (read < size) {
+        perror("Error: Failed to read file");
+        free(*output);
+        fclose(f);
+        return GENERIC_ERROR;
     }
 
     fclose(f);
-
-    //memcpy(p, &node, sizeof(char));
     return SUCCESS;
 }
 
