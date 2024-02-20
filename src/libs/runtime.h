f722b00f2abb0c7ca4dbc35f4df5e3e4827f7c9b
Benoît de Keyn <146000855+benoitdekeyn-algosup@users.noreply.github.com>
2024-02-20 14:10:15 +0100

    made the pieces of virtual electronic architechture

--- src/libs/runtime.h
+++ src/libs/runtime.h
@@ -6,9 +6,22 @@
 #include <string.h>
 #include <stdint.h>
 
-uint16_t registerArr[32];
+//__________________________________________________________________________________________________
+//  VIRTUAL COMPONENTS
+//_____________________
 
-uint8_t memory[201326591];
+uint16_t registerArr[32];     // the 32 registers
+
+// the memory
+uint8_t codeSection[33554432];  // 32 MB of the code section
+uint8_t dataSection[1073741824];  // 1 GB of the data section
+uint8_t inputSection[1024]; // 1 KB of the output section
+uint8_t outputSection[32768]; // 32 KB of the input section
+uint8_t videoSection[262144]; // 256 KB of the video section
+uint8_t stackSection[1048576]; // 1 MB of the stack section
+
+// No flags
+//__________________________________________________________________________________________________
 
 typedef enum binType{
     R,
