c765b137e43e7dec8672dd538a34a0d949f445c0
Léo CHARTIER <leo.chartier@algosup.com>
2024-02-23 14:45:21 +0100

    Move execute_inst from emulator to interpreter

--- src/libs/interpreter.h
+++ src/libs/interpreter.h
@@ -10,6 +10,7 @@ int machinecode_to_bininstruction(uint32_t, binInstruction_t*);
 int opcode_type_I(binInstruction_t*, uint8_t, uint8_t, uint8_t, int16_t);
 int opcode_type_J(binInstruction_t*, uint8_t, uint8_t, int32_t);
 int opcode_type_R(binInstruction_t*, uint8_t, uint8_t, uint8_t, uint8_t);
+int execute_instr(binInstruction_t);
 
 int read_bin(char* inputFile, uint8_t** outputPtr, uint32_t* size){
     FILE *f = fopen(inputFile, "rb");
@@ -260,6 +261,126 @@ int opcode_type_R(binInstruction_t* instruction, uint8_t opcode, uint8_t source,
         return GENERIC_ERROR;
     }
     return SUCCESS;
+}
+
+int execute_instr(binInstruction_t instruction)
+{
+    switch (instruction.type)
+    {
+        case (R):
+            switch (instruction.typeR.opcode)
+            {
+                case ADD:
+                    return instr_add(instruction);
+                case SUB:
+                    return instr_sub(instruction);
+                case MUL:
+                    return instr_mul(instruction);
+                case DIV:
+                    return instr_div(instruction);
+                case OR:
+                    return instr_or(instruction);
+                case AND:
+                    return instr_and(instruction);
+                case XOR:
+                    return instr_xor(instruction);
+                case ABS:
+                    return instr_abs(instruction);
+                case TLT:
+                    return instr_tlt(instruction);
+                case TLE:
+                    return instr_tle(instruction);
+                case TGT:
+                    return instr_tgt(instruction);
+                case TGE:
+                    return instr_tge(instruction);
+                case TEQ:
+                    return instr_teq(instruction);
+                case TNE:
+                    return instr_tne(instruction);
+                case STR:
+                    return instr_str(instruction);
+                case LD:
+                    return instr_ld(instruction);
+                case STRP:
+                    return instr_strp(instruction);
+                case LDP:
+                    return instr_ldp(instruction);
+                case PUSH:
+                    return instr_push(instruction);
+                case POP:
+                    return instr_pop(instruction);
+                case XCHG:
+                    return instr_xchg(instruction);
+                default:
+                    break;
+            }
+            break;
+
+        case (I):
+            switch (instruction.typeI.opcode)
+            {
+                case ADDI:
+                    return instr_addi(instruction);
+                case SUBI:
+                    return instr_subi(instruction);
+                case STRI:
+                    return instr_stri(instruction);
+                case LDI:
+                    return instr_ldi(instruction);
+                case ORI:
+                    return instr_ori(instruction);
+                case ANDI:
+                    return instr_andi(instruction);
+                case XORI:
+                    return instr_xori(instruction);
+                case SET:
+                    return instr_set(instruction);
+                case TLTI:
+                    return instr_tlti(instruction);
+                case TLEI:
+                    return instr_tlei(instruction);
+                case TGTI:
+                    return instr_tgti(instruction);
+                case TGEI:
+                    return instr_tgei(instruction);
+                case TEQI:
+                    return instr_teqi(instruction);
+                case TNEI:
+                    return instr_tnei(instruction);
+                default:
+                    break;
+            }
+            break;
+        
+        case (J):
+            switch (instruction.typeJ.opcode)
+            {
+                case B:
+                    return instr_b(instruction);
+                case BI:
+                    return instr_bi(instruction);
+                case BZ:
+                    return instr_bz(instruction);
+                case BNZ:
+                    return instr_bnz(instruction);
+                case CALL:
+                    return instr_call(instruction);
+                case CALLI:
+                    return instr_calli(instruction);
+                case RET:
+                    return instr_ret(instruction);
+                case JMP:
+                    return instr_jmp(instruction);
+                default:
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+    return GENERIC_ERROR;
 }
 
 #endif
